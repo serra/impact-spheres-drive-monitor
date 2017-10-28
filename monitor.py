a8bf5fb048ebd074df932d7925917f133094af90
Marijn van der Zee <marijn@serraict.com>
2017-10-28 17:15:43 +0200

    Add reporting functions

--- monitor.py
+++ monitor.py
@@ -55,13 +55,14 @@ def get_credentials():
     return credentials
 
 
-def iterate_files(service, folder_id, callback):
+def iterate_guides(service, folder_id, callback):
     page_token = None
     while True:
-        results = service.files().list(q="'" + folder_id + "' in parents",
+        results = service.files().list(q="'" + folder_id + "' in parents" +
+                                       " and mimeType = 'application/vnd.google-apps.document'",
                                        orderBy='name',
                                        pageSize=10,
-                                       fields='nextPageToken, files(id, name)',
+                                       fields='nextPageToken, files(id, name, modifiedTime, version)',
                                        pageToken=page_token).execute()
         files = results.get('files', [])
         for file in files:
@@ -71,13 +72,13 @@ def iterate_files(service, folder_id, callback):
             break
 
 
-def count_files(service, folder_id):
+def count_guides(service, folder_id):
     files = []
 
     def callback(file):
         files.append(file)
 
-    iterate_files(service, folder_id, callback)
+    iterate_guides(service, folder_id, callback)
 
     return len(files)
 
@@ -93,6 +94,10 @@ def get_process_folders(service):
         fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
     return items
+
+
+def get_review_folder(folders):
+    return next(f for f in folders if f['name'] == '3-In Review')
 
 
 def get_service(use_oauth=False):
@@ -114,25 +119,55 @@ def get_folders_with_file_counts(service):
         print('No files found.')
     else:
         for f in folders:
-            f['file_count'] = count_files(service, f['id'])
+            f['file_count'] = count_guides(service, f['id'])
     return folders
+
+
+def get_report_line(folder):
+    n = folder['file_count']
+    bar = '-' * min(n, 20)
+    if(n > 20):
+        bar = bar + ' + '
+    if(n == 0):
+        bar = '👏'
+    line = ' {0:<15} {1:>3} {2}'.format(folder['name'], n, bar)
+    return line
 
 
 def report_queues(folders):
     s = 'Guides: '
     for f in folders:
-        n = f['file_count']
-        bar = '-' * min(n, 20)
-        if(n > 20):
-            bar = bar + ' + '
-        s = '{3}\n{0:<15} {1:>3} {2}'.format(f['name'], n, bar, s)
+        s = '{0}\n{1}'.format(s, get_report_line(f))
+    return s
+
+
+def report_files_to_review(folders, service):
+    f = get_review_folder(folders)
+    url = 'https://drive.google.com/drive/u/0/folders/{0}'.format(f['id'])
+    guides = []
+
+    def callback(file):
+        guides.append(file)
+
+    iterate_guides(service, f['id'], callback)
+
+    s = 'Guides to review:'
+    for g in guides:
+        s = '{0}\n  {1}'.format(s, g['name'], url)
+
+    s = '{0} \n\nreview folder: {1}'.format(s, url)
     return s
 
 
 def main():
     service = get_service(use_oauth=True)
     folders = get_folders_with_file_counts(service)
+
+    print('```')
     print(report_queues(folders))
+    print('```')
+    print()
+    print(report_files_to_review(folders, service))
 
 
 if __name__ == '__main__':
