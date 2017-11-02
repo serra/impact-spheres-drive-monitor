9f2adeea7a5e53c7bcb2cbd01ec095629f4ca522
Marijn van der Zee <marijn@serraict.com>
2017-11-02 07:18:44 +0100

    Retrieve channel to notify from environment

--- notify.py
+++ notify.py
@@ -1,10 +1,11 @@
 from google.guide_reports import markdown_report
 from slack.post import send_message_to
-from slack.post import MARIJN_IM_ID
-from slack.post import IMPACT_SPHERES_ID
+import os
 
 
 def main():
+    channel = os.environ["SLACK_CHANNEL_TO_NOTIFY"]
+
     print('Retrieving report ... ')
     url = 'https://github.com/serra/impact-spheres-drive-monitor'
     footer = 'This message is posted automatically. ' + \
@@ -12,7 +13,7 @@ def main():
     msg = '{0}\n\n{1}'.format(markdown_report(), footer)
     print(msg)
     print('Sending report to agilityscales.slack.com ... ')
-    send_message_to(MARIJN_IM_ID, msg)
+    send_message_to(channel, msg)
     print('Done.')
 
 
