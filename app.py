55f08f91b6db26b9e29af513eb70257180e5f6f4
Marijn van der Zee <marijn@serraict.com>
2017-11-07 13:25:04 +0100

    The delayed reponse must be json formatted

--- app.py
+++ app.py
@@ -29,7 +29,10 @@ def echo():
 
 @async
 def post_report(response_url):
-    report = 'this will be the \n markdown-ish formatted report'  # markdown_report()
+    report = 'this will be the \n markdown-ish formatted report 👏'
+    # markdown_report()
+    data = {'text': report}
+
     print('posting report to <{0}>'.format(response_url))
-    requests.post(response_url, data=report.encode(encoding='utf-8'))
+    requests.post(response_url, json=data)
     pass
